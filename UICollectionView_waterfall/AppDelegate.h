70ffcfd14afe153bcb344fefccd3d38937a3e962
icy <icy.geng@you.world>
2017-05-12 23:06:06 +0800

    Initial Commit

---
+++ UICollectionView_waterfall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionView_waterfall
+//
+//  Created by icy on 2017/5/12.
+//  Copyright © 2017年 icy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionView_waterfall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionView_waterfall
+//
+//  Created by icy on 2017/5/12.
+//  Copyright © 2017年 icy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
